59f8db6cf8b3de3ffb4c42a57f1f3ed24ce012b9
Pete Gilbert <pete.gilbert16@gmail.com>
2020-06-23 19:09:35 -0400

    Updates

---
+++ precache-manifest.d5f019f6862aefd25f3fb700fd14fada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7324d4e59a90a0c21d1e030553a82c4c",
+    "url": "/peteGilbertProject5/index.html"
+  },
+  {
+    "revision": "1ec9846996865d07b4d6",
+    "url": "/peteGilbertProject5/static/css/main.64b3ad02.chunk.css"
+  },
+  {
+    "revision": "795e8ea6d8cc249060a1",
+    "url": "/peteGilbertProject5/static/js/2.5e874471.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/peteGilbertProject5/static/js/2.5e874471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec9846996865d07b4d6",
+    "url": "/peteGilbertProject5/static/js/main.eaefba58.chunk.js"
+  },
+  {
+    "revision": "e2472ac329892035d1de",
+    "url": "/peteGilbertProject5/static/js/runtime-main.e923f56a.js"
+  }
+]);
